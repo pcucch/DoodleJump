d8576420d357efa91c3bb66c9c6ec738b6f051fb
Tony Belladonna <belladon@cooper.edu>
2019-05-04 16:33:29 -0400

    pls work

---
+++ cmake-build-debug/CMakeFiles/DoodleJump.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DoodleJump.dir/main.cpp.o"
+  "DoodleJump.pdb"
+  "DoodleJump"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DoodleJump.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
