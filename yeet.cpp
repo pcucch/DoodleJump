10a7234dae09ba39350ade2755f4ed32ab736a04
Tony Belladonna <belladon@cooper.edu>
2019-05-04 01:04:35 -0400

    fixed

--- yeet.cpp
+++ yeet.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include <ncurses.h>			/* ncurses.h includes stdio.h */  
-#include <string.h> 
+#include <string.h>
+#include <unistd.h>            //Important for sleep function
  
 using namespace std;
+
+class Gameobject {
+public:
+    virtual void draw(WINDOW *win) const = 0;
+};
+
+class Doodler : public Gameobject {
+public:
+
+    int xpos, ypos;
+    int yVelocity;
+    int xspeed;
+
+    Doodler(){
+        xpos = 31;          //x position of doodler at start of the game
+        ypos = 10;          //y position of doodler at start of the game
+        yVelocity = 0;
+    }
+    void draw(WINDOW *win) const{
+        mvprintw(ypos, xpos, "X");
+    }
+};
+
 
 WINDOW *create_newwin(int height, int width, int starty, int startx)
 {	WINDOW *local_win;
@@ -16,16 +40,10 @@ WINDOW *create_newwin(int height, int width, int starty, int startx)
 	return local_win;
 }
 
-void destroy_win(WINDOW *local_win)
-{	
-	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-	wrefresh(local_win);
-	delwin(local_win);
-}
+int main()  {
 
-int main()
-{
 	WINDOW *gamewin;
+	Doodler doodler;
 	int startx, starty, width, height; 						 //Parameters for gamewin(dow)
 	int ch;
 	char mesg[]="Welcome to Doodle Jump! Press Any Button";				/* message to be appeared on the screen */
@@ -41,19 +59,31 @@ int main()
 	refresh();
 	getch();
 
-	keypad(stdscr, TRUE);
+	keypad(stdscr, TRUE);    //enables use of keypad on specified window
 	
 	height = 50;
-	width = 60;
+	width = 61;
 	starty = (LINES - height) / 2;							/* Calculating for a center placement */
 	startx = (COLS - width) / 2;							/* of the window		*/
 	printw("Press Q to exit");
 	refresh();
 	gamewin = create_newwin(height, width, starty, startx);
+	keypad(gamewin,TRUE);
 
-	while((ch = getch()) != 'q'){
-		keypad(gamewin, TRUE);
-	}
+	while((ch = getch()) != 'q') {                       //play game
+        usleep(50);
+        switch (ch) {
+            case KEY_LEFT:
+                doodler.xpos--;
+                break;
+            case KEY_RIGHT:
+                doodler.xpos++;
+                break;
+        }
+        doodler.draw(gamewin);
+        wrefresh(gamewin);
+        usleep(500);     //will stall the loop by one second
+    }
 	endwin();
 
 	return 0;
